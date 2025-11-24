370be19a48f22df3eec25510a73ae06a8d00fdaf
James Alvine <jamesalvine340@gmail.com>
2025-11-24 23:17:22 +0300

    account cached remoded

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,10 @@
 # backend/accounts/models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser  # ✅ MUST import this
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_super_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username']  # Keep username as a required field
